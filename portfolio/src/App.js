654cc761aa0a83a037ff72d66d95b6e875a2bc8d
hyesom2642 <kimhyesom@naver.com>
2022-06-01 00:23:32 +0900

    🚧 Implement Home component

---
+++ portfolio/src/App.js
@@ -0,0 +1,16 @@
+// > css 
+import './App.css';
+
+// > components
+import Home from './pages/Home';
+
+
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App;
