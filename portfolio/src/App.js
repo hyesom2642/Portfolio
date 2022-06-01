a7ae167e105f696ae28d63307bd6a9a89ed065c2
hyesom2642 <kimhyesom@naver.com>
2022-06-01 18:23:11 +0900

    🚩 Add responsive library

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -7,18 +7,39 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Project from './pages/Project';
 import EctButton from './components/EctButton';
+// import Responsive from './responsive/Responsive';
 
+// > mediaquery
+import { useMediaQuery } from 'react-responsive';
+import MobileHome from './responsive/Mobile/MobileHome';
+import MobileHeader from './responsive/Mobile/MobileHeader';
 
 function App() {
   return (
     <>
-      <Header />
-      <EctButton />
-      <Home />
-      <About />
-      <Project />
+      <Desktop>
+        <Header />
+        <EctButton />
+        <Home />
+        <About />
+        <Project />
+      </Desktop>
+      <Mobile>
+        <MobileHeader />
+        <MobileHome />
+      </Mobile>
     </>
   );
 }
 
 export default App;
+
+const Desktop = ({children}) => {
+  const isDesktop = useMediaQuery({ minWidth: 992 })
+  return isDesktop ? children : null
+}
+
+const Mobile = ({children}) => {
+  const isMobile = useMediaQuery({ maxWidth: 767 })
+  return isMobile ? children : null
+}
