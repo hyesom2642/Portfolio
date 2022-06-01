1a2bc99d6254b139b8682f888f60c93a945e4870
hyesom2642 <kimhyesom@naver.com>
2022-06-01 14:19:16 +0900

    🚧 Add components

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,13 +2,21 @@
 import './App.css';
 
 // > components
+import Header from './components/Header';
 import Home from './pages/Home';
+import About from './pages/About';
+import Project from './pages/Project';
+import EctButton from './components/EctButton';
 
 
 function App() {
   return (
     <>
+      <Header />
+      <EctButton />
       <Home />
+      <About />
+      <Project />
     </>
   );
 }
