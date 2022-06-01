49e06cfe84abb9d538239be7932477e92a05d598
hyesom2642 <kimhyesom@naver.com>
2022-06-01 14:09:45 +0900

    ✨ Implement sample of word part of About component

---
+++ portfolio/src/components/Word.js
@@ -0,0 +1,25 @@
+// > css 
+import styled from 'styled-components';
+
+function Word(){
+    return(
+        <>
+            <WordWrapper>
+                <p>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                </p>
+            </WordWrapper>
+        </>
+    )
+}
+
+export default Word;
+
+const WordWrapper = styled.div`
+    width: 800px;
+    padding: 30px 50px;
+    
+    p {
+        width: 100%;
+    }
+`;
